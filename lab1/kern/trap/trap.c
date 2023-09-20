a7f92e5e2239b5282d4db7d6277b18a558cad31f
duanjunqiku <3102275944@qq.com>
2023-09-20 23:09:02 +0800

    challenge3

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -10,6 +10,7 @@
 #include <trap.h>
 #include <sbi.h>
 
+
 #define TICK_NUM 100
 volatile size_t num=0;
 
@@ -106,21 +107,22 @@ void interrupt_handler(struct trapframe *tf) {
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   2110935 :  */
+             /* LAB1 EXERCISE2   YOUR CODE :  */
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
-            clock_set_next_event();
+            clock_set_next_event();//发生这次时钟中断的时候，我们要设置下一次时钟中断
             if (++ticks % TICK_NUM == 0) {
                 print_ticks();
-                __asm__ volatile ("ebreak");//为了观察断点异常是否能正常输出，我们在这里打印ticks的同时触发一个ebreak指令。
-                __asm__ volatile (".word 0x0");//为了观察违法指令异常是否能正常输出，我们在这里打印ticks的同时触发一个意⭐义⭐不⭐明的指令。
-                if (ticks == 1000)
-                    sbi_shutdown();
+                __asm("ebreak");
+                __asm("ecall");
+                __asm("mret");
             }
-            break;
+            if (ticks == 1000) {  // 打印次数为10时             
+                sbi_shutdown();  // 调用shut_down()函数关机
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
@@ -154,28 +156,25 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB1 CHALLENGE3   2110935 :  */
+             /* LAB1 CHALLENGE3   YOUR CODE :  */
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
-            //当前指令
-            cprintf("Illegal instruction at 0x%016llx\n", tf->epc);
-            cprintf("Exception type : Illegal instruction\n");
-            //RISC - V 中的 ebreak 指令通常是 2 字节长（16位），所以要+2而不是+4.
-            tf->epc += 2;
+            cprintf("Illegal instruction caught at 0x%x\n", tf->epc);
+            cprintf("Exception type: Illegal instruction\n");
+            tf->epc += 4; // 更新 tf->epc 寄存器
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB1 CHALLLENGE3   2110935 :  */
+            /* LAB1 CHALLLENGE3   YOUR CODE :  */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
-            cprintf("BREAKPOINT at 0x%016llx\n", tf->epc);
-            cprintf("Exception type : BREAKPOINT\n");
-            //RISC - V 中的 ebreak 指令通常是 2 字节长（16位），所以要+2而不是+4.
-            tf->epc += 2;
+            cprintf("ebreak caught at 0x%x\n", tf->epc);
+            cprintf("Exception type: breakpoint\n");
+            tf->epc += 4; // 更新 tf->epc 寄存器
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
@@ -198,7 +197,6 @@ void exception_handler(struct trapframe *tf) {
             break;
     }
 }
-
 /* trap_dispatch - dispatch based on what type of trap occurred */
 static inline void trap_dispatch(struct trapframe *tf) {
     if ((intptr_t)tf->cause < 0) {
