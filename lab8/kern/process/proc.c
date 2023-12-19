eab7e65a6b24a106feb8c94e1c0e57763e1a3b57
JohnnySwift <zkmysqh@163.com>
2023-12-19 23:53:41 +0800

    补全之前的代码

--- lab8/kern/process/proc.c
+++ lab8/kern/process/proc.c
@@ -127,7 +127,29 @@ alloc_proc(void) {
      * below fields(add in LAB6) in proc_struct need to be initialized
      *       struct files_struct * filesp;                file struct point        
      */
+        proc->state = PROC_UNINIT;
+    	proc->pid = -1;
+    	proc->runs = 0;
+    	proc->kstack = NULL;
+    	proc->need_resched = 0;
+        proc->parent = NULL;
+        proc->mm = NULL;
+        memset(&(proc->context), 0, sizeof(struct context));
+        proc->tf = NULL;
+        proc->cr3 = boot_cr3;
+        proc->flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN);
+        proc->wait_state = 0; //PCB新增的条目，初始化进程等待状态
+        proc->cptr = proc->optr = proc->yptr = NULL;//设置指针
+          proc->rq = NULL;
+        list_init(&(proc->run_link));
+        proc->time_slice = 0;
+        proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
+        proc->lab6_stride = 0;
+        proc->lab6_priority = 0;
+        proc->filesp = NULL;
     }
+
     return proc;
 }
 
@@ -223,6 +245,13 @@ proc_run(struct proc_struct *proc) {
         *   lcr3():                   Modify the value of CR3 register
         *   switch_to():              Context switching between two processes
         */
+        bool intr_flag;
+        struct proc_struct *prev = current, *next = proc;
+        local_intr_save(intr_flag);
+        current = proc;
+        lcr3(next->cr3);
+        switch_to(&(prev->context), &(next->context));
+        local_intr_restore(intr_flag);
        //LAB8 YOUR CODE : (update LAB4 steps)
       /*
      * below fields(add in LAB6) in proc_struct need to be initialized
@@ -469,6 +498,32 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
   *    update step 1: set child proc's parent to current process, make sure current process's wait_state is 0
   *    update step 5: insert proc_struct into hash_list && proc_list, set the relation links of process
     */
+      if ((proc = alloc_proc()) == NULL)
+        goto fork_out;
+    proc->parent = current;
+    assert(current->wait_state == 0); //确保进程在等待
+    if (setup_kstack(proc) == -E_NO_MEM)
+        goto bad_fork_cleanup_proc;
+
+    if (copy_mm(clone_flags, proc) != 0)
+        goto bad_fork_cleanup_kstack;
+
+    //好好研究一下为什么这个机掰stack是esp。
+    //这里的是父进程的stack pointer（上方注释），也就是栈指针，我们大致是可以把它当作esp使的。
+    copy_thread(proc, stack, tf);
+    bool interrupt_forbidden;
+    local_intr_save(interrupt_forbidden);
+    {
+        proc->pid = get_pid();
+        hash_proc(proc);
+        //list_add(&proc_list, &proc->list_link);
+        //nr_process++;
+        set_links(proc); //设置进程链接
+    }
+    //local_intr_restore就很明了了，如果原来是关着的那么就重新关着，如果本来是开着的那么就打开
+    local_intr_restore(interrupt_forbidden);
+    wakeup_proc(proc);
+    ret = proc->pid;
   
     if (copy_files(clone_flags, proc) != 0) { //for LAB8
         goto bad_fork_cleanup_kstack;
